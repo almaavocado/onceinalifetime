b458f217f617079bad2a1a639ab81b057f12e567
Alma Alvarado <almaalvarado@Almas-MacBook-Air.local>
2025-02-23 19:07:49 -0800

    removing video

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,48 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Once In a Lifetime Quiñcenera",
-  description: "Your App Description",
+  description: "Once In a Lifetime Quiñcenera",
+  icons: {
+    icon: "./assets/logo.png",
+  },
+  openGraph: {
+    title: "Once In a Lifetime Quiñcenera",
+    description: "Once In a Lifetime Quiñcenera",
+    url: "https://once-in-a-lifetime-quincenera.vercel.app",
+    siteName: "Once In a Lifetime Quiñcenera",
+    images: [
+      {
+        url: "./assets/logo.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Once In a Lifetime Quiñcenera",
+    description: "Once In a Lifetime Quiñcenera",
+    creator: "Alma Alvarado",
+    images: ["/logo.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  verification: {
+    google: "google",
+    yandex: "yandex",
+    yahoo: "yahoo",
+  },
 };
 
 export default function RootLayout({ children }) {
